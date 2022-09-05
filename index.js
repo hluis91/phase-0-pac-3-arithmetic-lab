118cfcef57405012f7dadda78724d22128ac7903
Luis Hernandez <lshrnndz6@gmail.com>
2022-09-04 20:55:14 -0700

    commit tree a5f5f6c136f98b66b0cea133033054be0ffdc445

---
+++ index.js
@@ -0,0 +1,125 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function increment(n){
+    return (n + 1);
+}
+
+function decrement(n){
+    return (n - 1);
+}
+
+function makeInt(string, n){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string, n){
+    return parseFloat(string, 10);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
